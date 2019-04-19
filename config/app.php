d88ed293151ade66fce885f871db1ddd4a7a8af3
luffyzhao <luffyzhao@vip.126.com>
2019-04-19 09:30:17 +0800

    LTools同步更新

--- config/app.php
+++ config/app.php
@@ -173,10 +173,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-
-        LTools\Providers\LaravelServiceProvider::class
-
+        App\Providers\RouteServiceProvider::class
     ],
 
     /*
